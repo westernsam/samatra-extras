0b864d1ede02c4074a2b5a26ff1287d0fb108674
sam <sam.owen.consultant@springer.com>
2017-05-22 17:41:30 +0100

    Sam: use implicit class to run request and record response for querying

--- src/main/scala/com/springer/samatra/extras/testing/SamatraControllerTestHelpers.scala
+++ src/main/scala/com/springer/samatra/extras/testing/SamatraControllerTestHelpers.scala
@@ -1,112 +1,279 @@
 package com.springer.samatra.extras.testing
 
-import java.io.BufferedReader
+import java.io._
+import java.net.URLEncoder
 import java.security.Principal
+import java.text.SimpleDateFormat
 import java.util
-import java.util.Locale
+import java.util.{Calendar, Locale, TimeZone}
+import java.util.concurrent.{ConcurrentHashMap, CopyOnWriteArrayList}
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger, AtomicReference}
+import java.util.function.Function
 import javax.servlet._
 import javax.servlet.http._
 
+import com.samskivert.mustache.Template
 import com.springer.samatra.routing.FutureResponses.FutureHttpResp
 import com.springer.samatra.routing.Request
-import com.springer.samatra.routing.Routings.{HttpResp, PathParamsRoute, RegexRoute, Routes}
+import com.springer.samatra.routing.Routings._
+import com.springer.samatra.routing.StandardResponses.{Halt, WithHeaders}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
 import scala.language.implicitConversions
 
 object SamatraControllerTestHelpers {
+  val dateFormat: SimpleDateFormat = new SimpleDateFormat("EEE, dd MM yyyy hh:mm:ss 'GMT'")
 
-  def httpServletRequest(path: String, method: String): HttpServletRequest = new HttpServletRequest {
-    override def getPathInfo: String = ???
-    override def getUserPrincipal: Principal = ???
-    override def getServletPath: String = ""
-    override def getDateHeader(name: String): Long = ???
-    override def getIntHeader(name: String): Int = ???
-    override def getMethod: String = method
-    override def getContextPath: String = ???
-    override def isRequestedSessionIdFromUrl: Boolean = ???
-    override def getPathTranslated: String = ???
-    override def getRequestedSessionId: String = ???
-    override def isRequestedSessionIdFromURL: Boolean = ???
-    override def logout(): Unit = ???
-    override def changeSessionId(): String = ???
-    override def getRequestURL: StringBuffer = ???
-    override def upgrade[T <: HttpUpgradeHandler](handlerClass: Class[T]): T = ???
-    override def getRequestURI: String = path
-    override def isRequestedSessionIdValid: Boolean = ???
-    override def getAuthType: String = ???
-    override def authenticate(response: HttpServletResponse): Boolean = ???
-    override def login(username: String, password: String): Unit = ???
-    override def getHeader(name: String): String = ???
-    override def getCookies: Array[Cookie] = ???
-    override def getParts: util.Collection[Part] = ???
-    override def getHeaders(name: String): util.Enumeration[String] = ???
-    override def getQueryString: String = ???
-    override def getPart(name: String): Part = ???
-    override def isUserInRole(role: String): Boolean = ???
-    override def getRemoteUser: String = ???
-    override def getHeaderNames: util.Enumeration[String] = ???
-    override def isRequestedSessionIdFromCookie: Boolean = ???
-    override def getSession(create: Boolean): HttpSession = ???
-    override def getSession: HttpSession = ???
-    override def getRemoteAddr: String = ???
-    override def getParameterMap: util.Map[String, Array[String]] = ???
-    override def getServerName: String = ???
-    override def getRemotePort: Int = ???
-    override def getParameter(name: String): String = ???
-    override def getRequestDispatcher(path: String): RequestDispatcher = ???
-    override def getAsyncContext: AsyncContext = ???
-    override def isAsyncSupported: Boolean = ???
-    override def getContentLength: Int = ???
-    override def getInputStream: ServletInputStream = ???
-    override def isAsyncStarted: Boolean = ???
-    override def startAsync(): AsyncContext = ???
-    override def startAsync(servletRequest: ServletRequest, servletResponse: ServletResponse): AsyncContext = ???
-    override def setCharacterEncoding(env: String): Unit = ???
-    override def getCharacterEncoding: String = ???
-    override def getServerPort: Int = ???
-    override def getAttributeNames: util.Enumeration[String] = ???
-    override def getContentLengthLong: Long = ???
-    override def getParameterNames: util.Enumeration[String] = ???
-    override def getContentType: String = ???
-    override def getLocalPort: Int = ???
-    override def getServletContext: ServletContext = null
-    override def getRemoteHost: String = ???
-    override def getLocalAddr: String = ???
-    override def getRealPath(path: String): String = ???
-    override def setAttribute(name: String, o: scala.Any): Unit = ???
-    override def getAttribute(name: String): AnyRef = ???
-    override def getLocales: util.Enumeration[Locale] = ???
-    override def removeAttribute(name: String): Unit = ???
-    override def getParameterValues(name: String): Array[String] = ???
-    override def getScheme: String = ???
-    override def getReader: BufferedReader = ???
-    override def isSecure: Boolean = ???
-    override def getProtocol: String = ???
-    override def getLocalName: String = ???
-    override def getDispatcherType: DispatcherType = ???
-    override def getLocale: Locale = ???
+  implicit class SamatraHttpRespHelpers(resp: HttpResp) {
+    val committed = new AtomicBoolean(false)
+    val bytes = new ByteArrayOutputStream()
+    val writer = new PrintWriter(new OutputStreamWriter(bytes))
+    val stream = new ServletOutputStream {
+      override def isReady: Boolean = true
+      override def setWriteListener(writeListener: WriteListener): Unit = throw new UnsupportedOperationException
+      override def write(b: Int): Unit = bytes.write(b)
+    }
+
+    val status = new AtomicInteger(200)
+    val respHeaders = new ConcurrentHashMap[String, CopyOnWriteArrayList[String]]()
+    val respCookies = new CopyOnWriteArrayList[Cookie]()
+    val characterEncoding = new AtomicReference[String]("UTF-8")
+    val contentType = new AtomicReference[String]()
+
+    resp.process(null, new HttpServletResponse {
+      override def sendError(sc: Int, msg: String): Unit = {
+        setStatus(sc)
+        getWriter.write(msg)
+      }
+      override def sendError(sc: Int): Unit = setStatus(sc)
+      override def getStatus: Int = status.get()
+
+      override def addCookie(cookie: Cookie): Unit = respCookies.add(cookie)
+
+      override def getHeader(name: String): String = respHeaders.get(name).asScala.head
+
+      override def setHeader(name: String, value: String): Unit = respHeaders.computeIfAbsent(name, _ => new CopyOnWriteArrayList[String]()).add(value)
+
+
+      override def setIntHeader(name: String, value: Int): Unit = setHeader(name, value.toString)
+      override def addDateHeader(name: String, date: Long): Unit = addHeader(name, formatDate(date))
+      override def setDateHeader(name: String, date: Long): Unit = setHeader(name, formatDate(date))
+
+      override def encodeURL(url: String): String = URLEncoder.encode(url, "UTF-8")
+      override def encodeUrl(url: String): String = URLEncoder.encode(url, "UTF-8")
+
+      override def addHeader(name: String, value: String): Unit = respHeaders.computeIfAbsent(name, _ => new CopyOnWriteArrayList[String]()).add(value)
+      override def getHeaders(name: String): util.Collection[String] = respHeaders.getOrDefault(name, new CopyOnWriteArrayList[String]())
+
+      override def encodeRedirectUrl(url: String): String = encodeUrl(url)
+      override def encodeRedirectURL(url: String): String = encodeURL(url)
+      override def sendRedirect(location: String): Unit = {
+        status.set(302)
+        addHeader("Location", location)
+      }
+      override def setStatus(sc: Int): Unit = status.set(sc)
+      override def setStatus(sc: Int, sm: String): Unit = {
+        status.set(sc)
+        getWriter.write(sm)
+      }
+      override def getHeaderNames: util.Collection[String] = respHeaders.keySet()
+      override def containsHeader(name: String): Boolean = respHeaders.containsKey(name)
+      override def addIntHeader(name: String, value: Int): Unit = addHeader(name, value.toString)
+      override def getBufferSize: Int = -1
+      override def resetBuffer(): Unit = ()
+      override def setContentType(`type`: String): Unit = contentType.set(`type`)
+      override def setBufferSize(size: Int): Unit = ()
+      override def isCommitted: Boolean = committed.get()
+      override def setCharacterEncoding(charset: String): Unit = characterEncoding.set(charset)
+      override def setContentLength(len: Int): Unit = setHeader("Content-Length", len.toString)
+      override def setContentLengthLong(len: Long): Unit = setHeader("Content-Length", len.toString)
+
+      override def getCharacterEncoding: String = characterEncoding.get
+      override def flushBuffer(): Unit = ()
+      override def getContentType: String = Option(contentType.get()).getOrElse("")
+      override def reset(): Unit = ()
+
+      override def getWriter: PrintWriter = {
+        if (committed.getAndSet(true)) throw new IllegalStateException("Connection already commited")
+        writer
+      }
+
+      override def getOutputStream: ServletOutputStream = {
+        if (committed.getAndSet(true)) throw new IllegalStateException("Connection already commited")
+        stream
+      }
+
+      override def getLocale: Locale = Locale.getDefault
+      override def setLocale(loc: Locale): Unit = ()
+    })
+
+    writer.flush()
+    writer.close()
+
+    stream.flush()
+    stream.close()
+
+    private def formatDate(date: Long) = {
+      val cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
+      cal.setTimeInMillis(date)
+      dateFormat.format(cal.getTime)
+    }
+
+    def outputAsString: String = new String(bytes.toByteArray)
+    def outputAsBytes: Array[Byte] = bytes.toByteArray
+    def outputAsStream: InputStream = new ByteArrayInputStream(outputAsBytes)
+
+    def statusCode: Int = status.get()
+
+    def headers: Map[String, Seq[String]] = {
+      val contentTypeHeaders = {
+        (Option(contentType.get), Option(characterEncoding.get())) match {
+          case (Some(t), Some(e)) => List(t, e)
+          case (Some(t), None) => List(t)
+          case (None, Some(e)) => List(e)
+          case (None, None) => List()
+        }
+      }
+      val hds = respHeaders.asScala.mapValues(_.asScala.toSeq).toMap
+      if (contentTypeHeaders.isEmpty) hds else hds.updated("Content-Type", contentTypeHeaders)
+    }
+
+    def cookies: Seq[Cookie] = respCookies.asScala
   }
+
+  /*
+    *
+    *
+    *
+      getParameter
+      getParameterMap
+
+      getAttribute
+      setAttribute
+      removeAttribute
+
+      getQueryString
+      getRequestURL
+
+      getCookies
+  [x] getInputStream
+    * */
+  def httpServletRequest(path: String, method: String, headers: Map[String, Seq[String]], body: Option[Array[Byte]]): HttpServletRequest = {
+    val bytes =  body match {
+      case Some(b) => new ByteArrayInputStream(body.get)
+      case None => new ByteArrayInputStream(new Array[Byte](0))
+    }
+
+    new HttpServletRequest {
+
+      override def getPathInfo: String = ???
+      override def getUserPrincipal: Principal = ???
+      override def getServletPath: String = ""
+      override def getDateHeader(name: String): Long = ???
+      override def getIntHeader(name: String): Int = ???
+      override def getMethod: String = method
+      override def getContextPath: String = "/"
+      override def isRequestedSessionIdFromUrl: Boolean = ???
+      override def getPathTranslated: String = ???
+      override def getRequestedSessionId: String = ""
+      override def isRequestedSessionIdFromURL: Boolean = ???
+      override def logout(): Unit = ()
+      override def changeSessionId(): String = ???
+      override def getRequestURL: StringBuffer = new StringBuffer(path)
+      override def upgrade[T <: HttpUpgradeHandler](handlerClass: Class[T]): T = ???
+      override def getRequestURI: String = path
+      override def isRequestedSessionIdValid: Boolean = ???
+      override def getAuthType: String = ???
+      override def authenticate(response: HttpServletResponse): Boolean = ???
+      override def login(username: String, password: String): Unit = ???
+      override def getHeader(name: String): String = ???
+      override def getCookies: Array[Cookie] = ???
+      override def getParts: util.Collection[Part] = ???
+      override def getHeaders(name: String): util.Enumeration[String] = ???
+      override def getQueryString: String = ???
+      override def getPart(name: String): Part = ???
+      override def isUserInRole(role: String): Boolean = ???
+      override def getRemoteUser: String = ???
+      override def getHeaderNames: util.Enumeration[String] = ???
+      override def isRequestedSessionIdFromCookie: Boolean = ???
+      override def getSession(create: Boolean): HttpSession = ???
+      override def getSession: HttpSession = ???
+      override def getRemoteAddr: String = ???
+      override def getParameterMap: util.Map[String, Array[String]] = ???
+      override def getServerName: String = ???
+      override def getRemotePort: Int = ???
+      override def getParameter(name: String): String = ???
+      override def getRequestDispatcher(path: String): RequestDispatcher = ???
+      override def getAsyncContext: AsyncContext = ???
+      override def isAsyncSupported: Boolean = ???
+      override def getContentLength: Int = ???
+      override def getInputStream: ServletInputStream = new ServletInputStream {
+        override def isReady: Boolean = true
+        override def isFinished: Boolean = bytes.available() > -1
+        override def setReadListener(readListener: ReadListener): Unit = ()
+        override def read(): Int = bytes.read()
+      }
+      override def isAsyncStarted: Boolean = ???
+      override def startAsync(): AsyncContext = ???
+      override def startAsync(servletRequest: ServletRequest, servletResponse: ServletResponse): AsyncContext = ???
+      override def setCharacterEncoding(env: String): Unit = ???
+      override def getCharacterEncoding: String = ???
+      override def getServerPort: Int = ???
+      override def getAttributeNames: util.Enumeration[String] = ???
+      override def getContentLengthLong: Long = ???
+      override def getParameterNames: util.Enumeration[String] = ???
+      override def getContentType: String = ???
+      override def getLocalPort: Int = ???
+      override def getServletContext: ServletContext = null
+      override def getRemoteHost: String = ???
+      override def getLocalAddr: String = ???
+      override def getRealPath(path: String): String = ???
+      override def setAttribute(name: String, o: scala.Any): Unit = ???
+      override def getAttribute(name: String): AnyRef = ???
+      override def getLocales: util.Enumeration[Locale] = ???
+      override def removeAttribute(name: String): Unit = ???
+      override def getParameterValues(name: String): Array[String] = ???
+      override def getScheme: String = "http"
+      override def getReader: BufferedReader = ???
+      override def isSecure: Boolean = false
+      override def getProtocol: String = ???
+      override def getLocalName: String = ???
+      override def getDispatcherType: DispatcherType = ???
+      override def getLocale: Locale = Locale.getDefault
+    }
+  }
+
+  def get(r: Routes)(path: String, headers: Map[String, Seq[String]] = Map.empty): Future[HttpResp] = runRequest(r, httpServletRequest(path, "GET", headers, None))
+  def head(r: Routes)(path: String, headers: Map[String, Seq[String]] = Map.empty): Future[HttpResp] = runRequest(r, httpServletRequest(path, "HEAD", headers, None))
+  def post(r: Routes)(path: String, headers: Map[String, Seq[String]] = Map.empty, body: Array[Byte]): Future[HttpResp] = runRequest(r, httpServletRequest(path, "POST", headers, Some(body)))
+  def put(r: Routes)(path: String, headers: Map[String, Seq[String]] = Map.empty, body: Array[Byte]): Future[HttpResp] = runRequest(r, httpServletRequest(path, "PUT", headers, Some(body)))
+  def delete(r: Routes)(path: String, headers: Map[String, Seq[String]] = Map.empty): Future[HttpResp] = runRequest(r, httpServletRequest(path, "DELETE", headers, None))
 
   def futureFrom(resp: HttpResp): Future[HttpResp] = resp match {
     case FutureHttpResp(fut, _, _, _, _) => fut.asInstanceOf[Future[HttpResp]]
     case _ => Future.successful(resp)
   }
 
-  def get(r: Routes)(path: String): Future[HttpResp] = runRequest(r, httpServletRequest(path, "GET"))
-  def head(r: Routes)(path: String): Future[HttpResp] = runRequest(r, httpServletRequest(path, "HEAD"))
-  def post(r: Routes, body: Array[Byte])(path: String): Future[HttpResp] = runRequest(r, httpServletRequest(path, "POST"))
-  def put(r: Routes)(path: String): Future[HttpResp] = runRequest(r, httpServletRequest(path, "PUT"))
-  def delete(r: Routes)(path: String): Future[HttpResp] = runRequest(r, httpServletRequest(path, "DELETE"))
-
   private def runRequest(r: Routes, httpReq: HttpServletRequest) = {
     val request = Request(httpReq)
-    val route = r.matching(httpReq, null).right.get
-    val requestToResp: (Request) => HttpResp = route match {
-      case PathParamsRoute(_, _, resp) => resp
-      case RegexRoute(_, _, resp) => resp
-    }
-    val matches = route.matches(request)
-    futureFrom(requestToResp(request.copy(params = matches.get)))
+
+    futureFrom(r.matching(httpReq, null) match {
+      case Right(route) =>
+        val requestToResp: (Request) => HttpResp = route match {
+          case PathParamsRoute(_, _, resp) => resp
+          case RegexRoute(_, _, resp) => resp
+        }
+        val matches = route.matches(request)
+        requestToResp(request.copy(params = matches.get))
+
+      case Left(matchingRoutes: Seq[Route]) => matchingRoutes match {
+        case Nil => Halt(404)
+        case _ =>
+          WithHeaders("Allow" -> Set(matchingRoutes.map(_.method): _*).mkString(", ")) {
+            Halt(405)
+          }
+      }
+    })
   }
 }
--- src/test/scala/com/springer/samatra/extras/testing/ExampleTest.scala
+++ src/test/scala/com/springer/samatra/extras/testing/ExampleTest.scala
@@ -1,25 +1,26 @@
 package com.springer.samatra.extras.testing
 
 import java.io.Reader
+import java.time.{Instant, LocalDate, ZoneOffset}
+import java.util.Date
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 import com.springer.samatra.extras.responses.JsonResponses.{JsonHttpResp, JsonResponse}
 import com.springer.samatra.extras.responses.XmlResponses.XmlResp
-import com.springer.samatra.extras.responses.{TemplateRenderer, TemplateResponse, ViewRenderingError}
+import com.springer.samatra.extras.responses.{MustacheRenderer, TemplateRenderer, TemplateResponse, ViewRenderingError}
 import com.springer.samatra.routing.Routings.Controller
-import com.springer.samatra.routing.StandardResponses.{AddCookie, StringResp, WithCookies, WithHeaders}
+import com.springer.samatra.routing.StandardResponses._
 import org.scalatest.FunSpec
 import org.scalatest.Matchers._
 import org.scalatest.concurrent.ScalaFutures
-import SamatraControllerTestHelpers.get
+import SamatraControllerTestHelpers._
 
+import scala.collection.{immutable, mutable}
 import scala.concurrent.Future
 
 class ExampleTest extends FunSpec with ScalaFutures {
 
-  implicit val fakeTemplates : TemplateRenderer = new TemplateRenderer {
-    override def rendered(viewName: String, model: Map[String, Any]): Either[ViewRenderingError, String] = Right(viewName)
-    override def rendered(reader: Reader, model: Map[String, Any]): Either[ViewRenderingError, String] = Right("reader")
-  }
+  implicit val renderer: TemplateRenderer = new MustacheRenderer(Map.empty, new MustacheRenderer.ClasspathTemplateLoader("."), false)
 
   val controllerUnderTest = new Controller {
 
@@ -42,6 +43,14 @@ class ExampleTest extends FunSpec with ScalaFutures {
       <hi>{req.captured("foo")}</hi>
     }
 
+    get("/req-response") { req =>
+      (req:HttpServletRequest, resp:HttpServletResponse) => {
+        resp.setDateHeader("Date", LocalDate.of(2017, 5, 18).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli)
+        resp.setStatus(200)
+        resp.getWriter.print("sam")
+      }
+    }
+
     get("/hello/:name") { req =>
       Future {
         WithCookies(Seq(AddCookie("cookie", "cookieVal"))) {
@@ -53,31 +62,62 @@ class ExampleTest extends FunSpec with ScalaFutures {
     }
   }
 
-  it("should test future string") {
-    whenReady(get(controllerUnderTest)("/hello/sam")) { result =>
-      result shouldBe WithCookies(Seq(AddCookie("cookie", "cookieVal"))) {
-        WithHeaders("a" -> "b") {
-          StringResp("sam")
+  describe("An example of unit testing controllers") {
+
+    it("should test future string") {
+      whenReady(get(controllerUnderTest)("/hello/sam")) { result =>
+        result shouldBe WithCookies(Seq(AddCookie("cookie", "cookieVal"))) {
+          WithHeaders("a" -> "b") {
+            StringResp("sam")
+          }
         }
       }
     }
-  }
 
-  it("should test json") {
-    whenReady(get(controllerUnderTest)("/json/sam")) { result =>
-      result shouldBe JsonHttpResp(JsonResponse(Map("foo" -> "sam")))
+    it("should test future string with ") {
+      whenReady(get(controllerUnderTest)("/req-response")) { result =>
+        result.statusCode shouldBe 200
+        result.headers.get("Date") shouldBe Some(List("Thu, 18 05 2017 01:00:00 GMT"))
+        result.outputAsString shouldBe "sam"
+      }
     }
-  }
 
-  it("should test xml") {
-    whenReady(get(controllerUnderTest)("/xml/sam")) { result =>
-      result shouldBe XmlResp("<hi>sam</hi>")
+    it("should test json") {
+      whenReady(get(controllerUnderTest)("/json/sam")) { result =>
+        result shouldBe JsonHttpResp(JsonResponse(Map("foo" -> "sam")))
+      }
     }
-  }
 
-  it("should test mustache") {
-    whenReady(get(controllerUnderTest)("/templated/hi")) { result =>
-      result shouldBe TemplateResponse("foo", Map("foo" -> "hi"))
+    it("should test xml") {
+      whenReady(get(controllerUnderTest)("/xml/sam")) { result =>
+        result shouldBe XmlResp("<hi>sam</hi>")
+      }
+    }
+
+    it("returns 404 on no match") {
+      whenReady(get(controllerUnderTest)("/nomatch")) { result =>
+        result shouldBe Halt(404)
+      }
+    }
+
+    it("returns 405 on on MethodNotAllowed") {
+      whenReady(put(controllerUnderTest)("/xml/sam", body = "body".getBytes)) { result =>
+        result shouldBe WithHeaders("Allow" -> "GET, HEAD") {
+          Halt(405)
+        }
+      }
+    }
+
+    it("blows up on no match") {
+      whenReady(get(controllerUnderTest)("/nomatch")) { result =>
+        result shouldBe Halt(404)
+      }
+    }
+
+    it("should test mustache") {
+      whenReady(get(controllerUnderTest)("/templated/hi")) { result =>
+        result shouldBe TemplateResponse("foo", Map("foo" -> "hi"))
+      }
     }
   }
 }
