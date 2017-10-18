8a96f71fd81965ce593cfe44795a37a9c718ff33
sam <sam.owen.consultant@springer.com>
2017-10-18 16:34:27 +0100

    Sam: metrics per route

--- src/main/scala/com/springer/samatra/extras/metrics/MetricsHandler.scala
+++ src/main/scala/com/springer/samatra/extras/metrics/MetricsHandler.scala
@@ -1,10 +1,10 @@
 package com.springer.samatra.extras.metrics
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
-import javax.servlet.{AsyncEvent, AsyncListener}
+import javax.servlet.{AsyncEvent, AsyncListener, ServletResponse}
 
-import com.springer.samatra.extras.Logger
-import com.timgroup.statsd.StatsDClient
+import com.springer.samatra.extras.metrics.MetricsHandler.isInternal
+import com.springer.samatra.routing.Routings.{PathParamsRoute, RegexRoute, Routes}
 import org.eclipse.jetty.server.handler.{AbstractHandler, HandlerWrapper}
 import org.eclipse.jetty.server.{AsyncContextEvent, HttpChannelState, Request}
 
@@ -12,22 +12,21 @@ object MetricsHandler {
   def isInternal(request: HttpServletRequest): Boolean = request.getRequestURI.startsWith("/internal")
 }
 
-class MetricsHandler(statsdClient: MetricsStatsdClient, handler: AbstractHandler, ignore: HttpServletRequest => Boolean) extends HandlerWrapper {
+abstract class BaseMetricsHandler(statsdClient: MetricsStatsdClient, handler: AbstractHandler, ignore: HttpServletRequest => Boolean) extends HandlerWrapper {
 
   setHandler(handler)
 
   val asyncRecorder = new AsyncListener {
     override def onError(event: AsyncEvent): Unit = ()
-    override def onComplete(event: AsyncEvent): Unit = record(event.asInstanceOf[AsyncContextEvent].getHttpChannelState.getBaseRequest)
+    override def onComplete(event: AsyncEvent): Unit = {
+      val state = event.asInstanceOf[AsyncContextEvent].getHttpChannelState
+      record(state.getBaseRequest, state.getServletResponse)
+    }
     override def onStartAsync(event: AsyncEvent): Unit = ()
     override def onTimeout(event: AsyncEvent): Unit = ()
   }
 
-  def record(req: Request) {
-    statsdClient.incrementCounter(s"webapp.responses.${responseCode(req)}xx")
-    val duration = System.currentTimeMillis - req.getTimeStamp
-    statsdClient.recordExecutionTime("webapp.responsetime", duration)
-  }
+  def record(req: Request, response: ServletResponse): Unit
 
   override def handle(target: String, request: Request, httpRequest: HttpServletRequest, httpResponse: HttpServletResponse): Unit = {
     if (!ignore(request)) statsdClient.incrementCounter("webapp.requests")
@@ -41,33 +40,35 @@ class MetricsHandler(statsdClient: MetricsStatsdClient, handler: AbstractHandler
         if (state.isInitial) {
           state.addListener(asyncRecorder)
         }
-      } else if (state.isInitial) record(request)
+      } else if (state.isInitial) record(request, httpResponse)
     }
   }
 
-  private def responseCode(request: Request): Int = request.getResponse.getStatus / 100
+  protected def responseCode(request: Request): Int = request.getResponse.getStatus / 100
 }
 
-trait MetricsStatsdClient {
-  def incrementCounter(name: String)
+class MetricsHandler(statsdClient: MetricsStatsdClient, handler: AbstractHandler, ignore: HttpServletRequest => Boolean = isInternal) extends BaseMetricsHandler(statsdClient, handler, ignore) {
 
-  def gauge(name: String, value: Long)
-
-  def recordExecutionTime(name: String, duration: Long)
+  def record(req: Request, response: ServletResponse): Unit = {
+    statsdClient.incrementCounter(s"webapp.responses.${responseCode(req)}xx")
+    val duration = System.currentTimeMillis - req.getTimeStamp
+    statsdClient.recordExecutionTime("webapp.responsetime", duration)
+  }
 }
 
-object MetricsStatsdClient extends Logger {
-  def newStatsDClient(statsd: StatsDClient, env: String) = new MetricsStatsdClient {
-    override def incrementCounter(name: String): Unit =
-      if (!env.equals("LOCAL")) statsd.incrementCounter(name)
-      else log.info(s"Incrementing metrics counter $name")
+class RouteMetricsHandler(routes: Routes, statsdClient: MetricsStatsdClient, handler: AbstractHandler, ignore: HttpServletRequest => Boolean = isInternal) extends BaseMetricsHandler(statsdClient, handler, ignore) {
 
-    override def gauge(name: String, value: Long): Unit =
-      if (!env.equals("LOCAL")) statsd.gauge(name, value)
-      else log.info(s"Recording gauge metric ($name, $value)")
+  def record(req: Request, response: ServletResponse) {
+    val routeName: Option[String] = routes.matching(req, response.asInstanceOf[HttpServletResponse]) match {
+      case Right(PathParamsRoute(_, path, _)) => Some(path)
+      case Right(RegexRoute(_, pattern, _)) => Some(pattern.toString())
+      case _ => None
+    }
 
-    override def recordExecutionTime(name: String, duration: Long): Unit =
-      if (!env.equals("LOCAL")) statsd.recordExecutionTime(name, duration)
-      else log.info(s"Recording execution time metric ($name, $duration)")
+    routeName.foreach { pattern =>
+      statsdClient.incrementCounter(s"webapp.$pattern.responses.${responseCode(req)}xx")
+      val duration = System.currentTimeMillis - req.getTimeStamp
+      statsdClient.recordExecutionTime(s"webapp.$pattern.responsetime", duration)
+    }
   }
 }
